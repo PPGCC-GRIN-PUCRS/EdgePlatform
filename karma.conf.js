a9aa76876c296b83e98037b5d6823ab2fe860b4f
Cleyson Oliveira <cleyson.oliveira@pucrs.br>
2025-01-15 11:47:53 -0300

    Remove phantomjs from karma config

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function (config) {
       require("karma-jasmine"),
       require("karma-coverage"),
       require("karma-chrome-launcher"),
-      require("karma-phantomjs-launcher"),
+      // require("karma-phantomjs-launcher"),
       require("karma-jasmine-html-reporter"),
       require("@angular-devkit/build-angular/plugins/karma"),
     ],
